7a6eb390a8e092ea5c75f99a2f01b013e05d4923
komyak9 <komyak9@gmail.com>
2022-03-20 11:54:54 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+
+
+// 2аб
+//Система нелинейных уравнений - метод простых итераций
+//Решение нелинейных уравнений - метод деления пополам, метод хорд
